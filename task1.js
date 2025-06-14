4aeaca14a638fb8e404ab55ec33b6a32150de3d9
Andrii <fandriy85@gmail.com>
2025-06-14 18:08:22 -0400

    add task

--- task1.js
+++ task1.js
@@ -1,7 +1,21 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const usersList = document.querySelector(".usersList");
+
+async function render() {
+  const resp = await fetch("https://jsonplaceholder.typicode.com/users", {});
+  const users = await resp.json();
+  //   const userNames = users.map(({ name }) => name);
+  for (const user of users) {
+    const li = document.createElement("li");
+    li.textContent = user.name;
+    usersList.appendChild(li);
+  }
+}
+render();
--- task2.js
+++ task2.js
@@ -5,3 +5,27 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const btn = document.querySelector("#getUserButton");
+const inputName = document.querySelector("#userNameInput");
+const userCity = document.querySelector("#userCity");
+
+btn.addEventListener("click", async () => {
+  try {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/users", {});
+    if (!resp.ok) {
+      throw new Error("HTTP error!");
+    }
+
+    const usersObj = await resp.json();
+    const foundUser = usersObj.find((user) => user.name === inputName.value);
+    if (foundUser) {
+      userCity.textContent = `${foundUser.address.city} `;
+    } else {
+      userCity.textContent = "User not found";
+    }
+  } catch (error) {
+    console.error(error);
+    userCity.textContent = error.message;
+  }
+});
